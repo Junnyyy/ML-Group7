073310bd22d9bc07e34ee3400b934e18e19b9622
Johnny <44646589+Junnyyy@users.noreply.github.com>
2022-11-23 13:05:44 -0600

    🗑️ Cleanup code

--- DecisionTree.r
+++ DecisionTree.r
@@ -1,28 +1,46 @@
-install.packages('readr')
-install.packages('tree')
-install.packages('randomForest')
-#install.packages('gbm')
+install.packages("readr")
+install.packages("tree")
+install.packages("randomForest")
+# install.packages('gbm')
 library(readr)
 library(tree)
 library(randomForest)
-#library(gbm)
+# library(gbm)
 
 # Load in health Data
-health <- read_csv("healthcare-dataset-stroke-data.csv", col_types = cols(gender = col_factor(levels = c("Male", "Female", "Other")), hypertension = col_factor(levels = c("0", "1")), heart_disease = col_factor(levels = c("0", "1")), ever_married = col_factor(levels = c("No", "Yes")), work_type = col_factor(levels = c("children", "Govt_job", "Never_worked", "Private", "Self-employed")), Residence_type = col_factor(levels = c("Rural", "Urban")), bmi = col_number(), smoking_status = col_factor(levels = c("formerly smoked", "never smoked", "smokes")), stroke = col_factor(levels = c("0", "1"))))
+health <- read_csv("healthcare-dataset-stroke-data.csv",
+  col_types = cols(
+    gender = col_factor(levels = c("Male", "Female", "Other")),
+    hypertension = col_factor(levels = c("0", "1")),
+    heart_disease = col_factor(levels = c("0", "1")),
+    ever_married = col_factor(levels = c("No", "Yes")),
+    work_type = col_factor(
+      levels =
+        c("children", "Govt_job", "Never_worked", "Private", "Self-employed")
+    ),
+    Residence_type = col_factor(levels = c("Rural", "Urban")),
+    bmi = col_number(),
+    smoking_status = col_factor(
+      levels =
+        c("formerly smoked", "never smoked", "smokes")
+    ),
+    stroke = col_factor(levels = c("0", "1"))
+  )
+)
 # Remove ID column
-health <- health[,-1]
+health <- health[, -1]
 # Omit NA values
 health <- na.omit(health)
-View(health)
+# View(health)
 
 
 # Create training data
 set.seed(500)
-train <- sample(1:nrow(health), nrow(health)/2 + 0.5)
-health.train <- health[train,]
+train <- sample(1:nrow(health), nrow(health) / 2 + 0.5)
+health.train <- health[train, ]
 
 # Create testing data
-health.test <- health[-train,]
+health.test <- health[-train, ]
 
 # Create Decision Tree
 health.tree <- tree(stroke ~ ., data = health.train)
@@ -38,7 +56,7 @@ table(health.pred, health.test$stroke)
 
 
 # Prune Decision Tree
-cv.health = cv.tree(health.tree, FUN = prune.misclass)
+cv.health <- cv.tree(health.tree, FUN = prune.misclass)
 plot(cv.health$size, cv.health$dev, type = "b")
 
 # Predict using Pruned Tree
@@ -48,9 +66,9 @@ table(health.pred.pruned, health.test$stroke)
 
 
 # Bagged Model
-p = ncol(health)
-n = nrow(health)
-B = 1000
+p <- ncol(health)
+n <- nrow(health)
+B <- 1000
 health.bag <- randomForest(stroke ~ ., data = health.train, mtry = p, ntree = B, importance = TRUE)
 health.bag
 
@@ -70,9 +88,9 @@ text(health.tree.bag, pretty = 0)
 # Random Forest Variables
 STROKE.test <- as.factor(health.train$stroke)
 X.test <- health[-train, -11]
-p = ncol(health)
-n = nrow(health)
-B = 1000
+p <- ncol(health)
+n <- nrow(health)
+B <- 1000
 
 # Random Forest Model
 health.rf <- randomForest(stroke ~ ., data = health, subset = train, xtest = X.test, ytest = STROKE.test, mtry = sqrt(p), ntree = B, importance = TRUE, keep.forest = TRUE)
@@ -86,7 +104,8 @@ table(health.pred.rf, health.test$stroke)
 varImpPlot(health.rf)
 
 # Create new decision tree based on random forest model
-health.tree.rf <- tree(stroke ~ ., data = health.train, subset = health.rf$inbag)plot(health.tree.rf)
+health.tree.rf <- tree(stroke ~ ., data = health.train, subset = health.rf$inbag)
+plot(health.tree.rf)
 text(health.tree.rf, pretty = 0)
 
 # Boosted Model
